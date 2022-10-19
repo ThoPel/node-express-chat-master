a11006beb4ce4c0663ba2e242edab920a594b966
PELFRENE <87179999+ThoPel@users.noreply.github.com>
2022-10-19 16:27:27 +0200

    Add feature to send message in the chat

--- back-end/models/message.js
+++ back-end/models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
   text: { type: String, required: true },
-  userId: { type: Number, required: true },
+  userId: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Message', messageSchema);
