ff8168a70766c130abc813303f11b410fbaa4f83
PELFRENE <87179999+ThoPel@users.noreply.github.com>
2022-10-19 15:46:15 +0200

    Add get User name

--- front-end/main.js
+++ front-end/main.js
@@ -2,7 +2,7 @@
     const server = 'http://localhost:3000'
     const socket = io(server);
 
-    window.localStorage.setItem('id', 'test')
+    window.localStorage.setItem('id', '634fecaf5c7d276a2a4dc0fe')
 
 
     socket.on('notification', (data) => {
